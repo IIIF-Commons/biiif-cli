3362727373b79d2e002ba4eed6778e1336a5d35e
edsilv <e.silverton@gmail.com>
2018-04-30 16:53:04 +0100

    scaffold readme

--- index.js
+++ index.js
@@ -16,23 +16,45 @@ async function execCli(env, options) {
 
 	const dir = program.args[0];
 
-	if (program.generateFiles) {
+	if (program.scaffoldFiles) {
 		console.log('scaffolding files');
 
-		if (!fs.existsSync(dir)){
+		if (!fs.existsSync(dir)) {
 			fs.mkdirSync(dir);
 		}
  
 		ncp.limit = 16;
+
+		const scaffoldTarget = './scaffoldtest';
 		
-		ncp(source, destination, function (err) {
+		ncp('./scaffold', scaffoldTarget, function (err) {
 			if (err) {
 				return console.error(err);
 			}
+
+			// replace [url]
+			await replaceInFile('./README.md', /[url]/g, program.url);
+			await replaceInFile('./index.html', /[url]/g, program.url);
+
 			console.log('finished scaffolding');
 		});
-		//await exec('mkdir collection');
 	}
 
 	build(dir, program.url);
+}
+
+async function replaceInFile(file, replacetarget, replacewith) {
+	return fs.readFile(file, 'utf8', function (err,data) {
+		if (err) {
+		    return console.log(err);
+		}
+
+		const result = data.replace(replacetarget, replacewith);
+	  
+		fs.writeFile(file, result, 'utf8', function (err) {
+		   if (err) {
+			   return console.log(err);
+		   }
+		});
+	});
 }
